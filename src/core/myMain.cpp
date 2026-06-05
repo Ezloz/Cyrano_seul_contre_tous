5c107d6e9bc973e8ee7c44cc59e3f9f10fbb1b99
Kaou S <corentin.sizgoric@gmail.com>
2026-06-05 10:25:38 +0200

    feat: Adapting MyMain.cpp for game, without Layers

--- src/core/myMain.cpp
+++ src/core/myMain.cpp
@@ -4,11 +4,24 @@
 
 #include <SFML/Graphics.hpp>
 
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/Keyboard.hpp>
+
 
 using namespace std;
 int myMain() {
 
-  sf::RenderWindow window{sf::VideoMode({800, 800}), "SFML works!"};
+  sf::RenderWindow window{sf::VideoMode({800, 600}), "SFML works!"};
+  tmx::Map map;
+  // map.load("res/demo.tmx");
+  //
+  // MapLayer layerZero(map, 0);
+  // MapLayer layerOne(map, 1);
+  // MapLayer layerTwo(map, 2);
+
+  sf::Clock globalClock;
+  sf::Clock wiggleClock;
   window.setFramerateLimit(30);
 
   sf::RectangleShape white_rectangle{sf::Vector2f(800,800)};
@@ -19,7 +32,10 @@ int myMain() {
     // iteration of the loop
     while (const std::optional event = window.pollEvent()) {
       // "close requested" event: we close the window
-      if (event->is<sf::Event::Closed>()) window.close();
+        if (event->is<sf::Event::Closed>())
+        {
+            window.close();
+        }
       
       // Catch the resize events
       if (const auto *resized = event->getIf<sf::Event::Resized>()) {
@@ -29,10 +45,34 @@ int myMain() {
         sf::FloatRect visibleArea({0.f, 0.f}, sf::Vector2f(resized->size));
         window.setView(sf::View(visibleArea));
       }
+
+      else if(const auto *keyPressed = event->getIf<sf::Event::KeyPressed>())
+      {
+          switch(keyPressed->code)
+          {
+              case sf::Keyboard::Key::W:
+                  doWiggle = !doWiggle;
+              break;
+          }
+      }
+
     }
-    window.clear();
-    
-    window.draw(white_rectangle);
+    sf::Time duration = globalClock.restart();
+    // layerZero.update(duration);
+
+    sf::Vector2f newOffset = sf::Vector2f(0.f, 0.f);
+    if (doWiggle)
+    {
+          newOffset = sf::Vector2f(std::cos(wiggleClock.getElapsedTime().asSeconds()) * 100.f, 0.f);
+    }
+    // layerZero.setOffset(newOffset);
+    // layerOne.setOffset(newOffset);
+    // layerTwo.setOffset(newOffset);
+
+    window.clear(sf::Color::Black);
+    // window.draw(layerZero);
+    // window.draw(layerOne);
+    // window.draw(layerTwo);
     window.display();
   }
 
