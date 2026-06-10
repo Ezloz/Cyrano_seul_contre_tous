30be64192da696a0b563f52ddbdf7fcbc8e1d244
Ezloz <95761894+Ezloz@users.noreply.github.com>
2026-06-10 16:01:04 +0200

    Adding empty files + Working on GameApp

---
+++ src/core/GameApp.cpp
@@ -0,0 +1,33 @@
+Singleton* Singleton::singleton_= nullptr;;
+
+/**
+ * Static methods should be defined outside the class.
+ */
+GameApp *GameApp::GetInstance(const std::string& value)
+{
+    /**
+     * This is a safer way to create an instance. instance = new Singleton is
+     * dangeruous in case two instance threads wants to access at the same time
+     */
+    if(singleton_==nullptr){
+        singleton_ = new Singleton(value);
+    }
+    return singleton_;
+}
+
+
+
+int main()
+{
+    std::cout <<"If you see the same value, then singleton was reused (yay!\n" <<
+                "If you see different values, then 2 singletons were created (booo!!)\n\n" <<
+                "RESULT:\n";   
+    std::thread t1(ThreadFoo);
+    std::thread t2(ThreadBar);
+    t1.join();
+    t2.join();
+
+    return 0;
+}
+    
+};
---
+++ src/core/GameApp.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+
+enum Input{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    CONFIRM,
+    CANCEL,
+    MENU,    
+};
+
+
+// Singleton format taken from refactoring.guru 
+class GameApp {
+
+    /**
+     * The Singleton's constructor should always be private to prevent direct
+     * construction calls with the `new` operator.
+     */
+private:
+    GameApp() {LoadOptions();};
+
+    static GameApp* gameapp_;
+
+    void LoadOptions();
+
+    std::vector<Input> inputs;
+
+    deltatime;
+
+public:
+
+    /**
+     * Singletons should not be cloneable.
+     */
+    GameApp(GameApp &other) = delete;
+    /**
+     * Singletons should not be assignable.
+     */
+    void operator=(const GameApp &) = delete;
+    /**
+     * This is the static method that controls the access to the singleton
+     * instance. On the first run, it creates a singleton object and places it
+     * into the static field. On subsequent runs, it returns the client existing
+     * object stored in the static field.
+     */
+
+    static GameApp *GetInstance(const std::string& value);
+    /**
+     * Finally, any singleton should define some business logic, which can be
+     * executed on its instance.
+     */
+
+    auto GetInputs() {return this->inputs};
+
+    void SomeBusinessLogic()
+    {
+        // ...
+    }
+
+};
+
